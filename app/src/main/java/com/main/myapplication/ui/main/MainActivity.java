a20324209382d25699dec839c1d25589d7db070e
meshnesh <tonnie.nesh@gmail.com>
2019-09-03 10:45:26 +0300

    added logout menu

--- app/src/main/java/com/main/myapplication/ui/main/MainActivity.java
+++ app/src/main/java/com/main/myapplication/ui/main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.main.myapplication.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -18,5 +21,27 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                sessionManager.logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
